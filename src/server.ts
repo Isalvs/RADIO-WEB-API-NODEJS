cbd17153f198077b0446b17b72b02b198e5e7116
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-01 13:22:55 -0500

    update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import routes from './routes';
 import io from 'socket.io'
-import http from 'http';
+import https from 'https';
 import knex from '../src/database/connection';
 const app = express();
 app.use(express.json());
 app.use(routes);
-const server = http.createServer(app);
+const server = https.createServer(app);
 const IO = io.listen(server)
 
 IO.on("connection", socket => {
