48e95fc1313f6a95f983a758e4644204c9ef559f
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-02 12:09:20 -0500

    upgrade project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import routes from './routes';
 import cors from 'cors';
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(routes);
-var port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log('server running', port));
