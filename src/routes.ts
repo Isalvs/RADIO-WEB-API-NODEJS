a0ee7e4258a2651c50b7e959643efe586ed93431
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-02 12:16:14 -0500

    upgrade project

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import UserController from './controllers/UserController';
 import SongsController from './controllers/SongsController';
-import ChatController from './controllers/ChatController';
 const routes = express.Router();
 
 const userController = new UserController();
 const songsController = new SongsController();
-const chatController = new ChatController();
-
 
 routes.get('/musics', songsController.index);
 routes.post('/musics/create', songsController.create);
@@ -19,7 +16,5 @@ routes.post('/users/create', userController.create);
 routes.post('/users/email_send', userController.sendEmail);
 routes.post('/users/validation', userController.validationEmail);
 
-routes.get('/messages', chatController.index);
-routes.post('/message/create', chatController.create);
 
 export default routes;
