cc44c0de585d9c9ef38eb8dfd6502219495a162c
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-01 22:04:25 -0500

    update

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,9 @@
 import express from 'express';
 import routes from './routes';
-//import io from 'socket.io'
-//import http from 'http';
 import cors from 'cors';
-//import knex from '../src/database/connection';
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(routes);
-
-// const server = http.createServer(app);
-// const IO = io.listen(server)
-
-// IO.on("connection", socket => {
-//     console.log("a user connected :D");
-//     socket.on("chat message", msg => {
-//         console.log(msg);
-//         IO.emit("chat message", msg);
-//         knex('roomchat').insert({
-//             userID: msg.userID,
-//             text: msg.text,
-//             name: msg.name,
-//         }).then(e=>{
-//             console.log(e)
-//         }).catch(e=> console.log(e))
-//     });
-// })
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log('server running', port));
--- src/socket/io.ts
+++
@@ -1,6 +0,0 @@
-
-async function writeServerDatabase(msm:[]){
-
-}
-
-export default writeServerDatabase;
